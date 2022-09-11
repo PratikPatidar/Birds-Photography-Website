fa4f57e8fb4d8019ba56c3e6a0153d8d8b6e6bb8
Pratik Patidar <pratikpatidar5350@gmail.com>
2022-09-11 22:54:18 +0530

    Initial commit

---
+++ node.js
@@ -0,0 +1,8 @@
+
+let contact = document.getElementById(Contact)
+console.log("hi");
+
+// let contact = document.querySelectorAll(Contact);
+let value = contact.inner.HTML;
+console.log(contact);
+
